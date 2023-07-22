98714e3cef976d82d257fd942891df315f1cf028
İsa Can Çabuk <isacancabuk@gmail.com>
2023-07-22 18:01:53 +0300

    code is written for app.js

--- first-project/my-first-app/src/App.js
+++ first-project/my-first-app/src/App.js
@@ -1,5 +1,35 @@
 function App() {
-  
+  const testArray = [
+    {
+      value: "volvo",
+      name: "Volvo",
+    },
+    {
+      value: "fiat",
+      name: "Fiat",
+    },
+    {
+      value: "mercedes",
+      name: "Mercedes",
+    },
+    {
+      value: "audi",
+      name: "Audi",
+    },
+  ];
+
+  return (
+    <main>
+      <label htmlFor="cars">Choose a car:</label>
+      <select name="cars" id="cars">
+        {testArray.map((item) => (
+          <option key={item.value} value={item.value}>
+            {item.name}
+          </option>
+        ))}
+      </select>
+    </main>
+  );
 }
 
 export default App;
